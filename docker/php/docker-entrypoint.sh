eb1c709ce237a821830ad3faf46ac149094b142f
Alexandre HUBERT <alexandre.hubert@niji.fr>
2024-06-27 11:54:01 +0200

    update README

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -6,9 +6,13 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php "$@"
 fi
 
-if [ "$FOLDER" != "" ]; then
-  cd /var/www/html/"$FOLDER"
-  symfony server:start --port=80 --no-tls
+dirname="/var/www/html/$FOLDER"
+
+if [ -d "$dirname" ]; then
+    cd "$dirname"
+    symfony server:start --port=80 --no-tls
+else
+    echo "$dirname does not exist, or is not a directory. Server Web Not start"
 fi
 
 exec "$@"
